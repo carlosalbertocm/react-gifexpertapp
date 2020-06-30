a24c1270b2b09e8ab3ca48754fecd8632b66073d
Carlos Conde <carlosacondemoreno@gmail.com>
2020-06-30 12:46:03 -0500

    Use Effect - mostrando el titulo de las imagenes

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { GifGridItem } from "./GifGridItem";
 
 export const GifGrid = ({ category }) => {
+	const [images, setImages] = useState([]);
+
+	useEffect(() => {
+		getGifs();
+	}, []);
+
 	const getGifs = async () => {
 		const apiKey = "DiwjgpmnTacHgHTFdpK49FofZluO2IMG";
 		const url = `https://api.giphy.com/v1/gifs/search?q=dragon+ball&limit=20&api_key=${apiKey}`;
@@ -14,9 +21,15 @@ export const GifGrid = ({ category }) => {
 			};
 		});
 
-		console.log(gifs);
+		setImages(gifs);
 	};
-	getGifs();
 
-	return <div>{category}</div>;
+	return (
+		<div>
+			<h3>{category}</h3>
+			{images.map((img) => (
+				<GifGridItem key={img.id} {...img} />
+			))}
+		</div>
+	);
 };
