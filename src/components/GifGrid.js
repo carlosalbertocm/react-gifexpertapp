63cdeba159c2cffe3fd3e656f0ce15096d305ca3
Carlos Conde <carlosacondemoreno@gmail.com>
2020-06-30 15:34:11 -0500

    Custom Hook - creando un hook personalizado para la carga de gifs

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,22 +1,25 @@
-import React, { useState, useEffect } from "react";
-import { GifGridItem } from "./GifGridItem";
-import { getGifs } from "../helpers/getGifs";
+import React from "react";
+import { useFetchGifs } from "../hooks/useFetchGifs";
+// import { GifGridItem } from "./GifGridItem";
+// import { getGifs } from "../helpers/getGifs";
 
 export const GifGrid = ({ category }) => {
-	const [images, setImages] = useState([]);
+	// const [images, setImages] = useState([]);
+	const { loading } = useFetchGifs();
 
-	useEffect(() => {
-		getGifs(category).then((imgs) => setImages(imgs));
-	}, [category]);
+	// useEffect(() => {
+	// 	getGifs(category).then((imgs) => setImages(imgs));
+	// }, [category]);
 
 	return (
 		<>
 			<h3>{category}</h3>
-			<div className="card-grid">
+			{loading ? "Cargando..." : "Fin de carga"}
+			{/* <div className="card-grid">
 				{images.map((img) => (
 					<GifGridItem key={img.id} {...img} />
 				))}
-			</div>
+			</div> */}
 		</>
 	);
 };
