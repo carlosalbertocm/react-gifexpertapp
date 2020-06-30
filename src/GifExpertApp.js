0b09d52472c7dc444a3f2f5988de8eaf4443e267
Carlos Conde <carlosacondemoreno@gmail.com>
2020-06-30 11:34:29 -0500

    Creacion de lista de categorias

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const GifExpertApp = () => {
+	// const categories = ["One Punchy", "Samurai X", "Dragon Ball"];
+
+	const [categories, setCategories] = useState([
+		"One Punchy",
+		"Samurai X",
+		"Dragon Ball",
+	]);
+
+	const handleAdd = () => {
+		// setCategories(["HunterXHunter", ...categories]);
+		setCategories((cats) => [...categories, "HunterXHunter"]);
+	};
+
 	return (
 		<>
 			<h2>GifExpertApp </h2>
 			<hr />
+			<button onClick={handleAdd}>Agregar</button>
+			<ol>
+				{categories.map((category) => {
+					return <li key={category}>{category}</li>;
+				})}
+			</ol>
 		</>
 	);
 };
