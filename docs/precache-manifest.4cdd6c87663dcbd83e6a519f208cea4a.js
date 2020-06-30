6fd2d1874d9b8d33dff7a597c13dcb65ba4fcc03
Carlos Conde <carlosalbertocondemoreno@gmail.com>
2020-06-30 16:23:27 -0500

    Adding docs folder

---
+++ docs/precache-manifest.4cdd6c87663dcbd83e6a519f208cea4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3155bbf640d502b5e7e51c718adb0c3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b76b62194b1280b5f026",
+    "url": "/static/css/main.efd9d393.chunk.css"
+  },
+  {
+    "revision": "d26de92f2f1110139d50",
+    "url": "/static/js/2.a53b6e1d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a53b6e1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b76b62194b1280b5f026",
+    "url": "/static/js/main.bde2c31b.chunk.js"
+  },
+  {
+    "revision": "72e4b1f2ff4246389bc8",
+    "url": "/static/js/runtime-main.a50afc49.js"
+  }
+]);
