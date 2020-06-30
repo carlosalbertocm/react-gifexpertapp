b98179c947612ee44dcbcb52004b4fda4c1307ce
Carlos Conde <carlosacondemoreno@gmail.com>
2020-06-30 15:45:01 -0500

    useFetchGifs - Obteniendo imagenes y bandera de carga
    Afinando el custom hook, limpiando el componente principal GifGrid,
    quitandole las funciones que las hace mas pesada usando custom hook

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,25 +1,19 @@
 import React from "react";
 import { useFetchGifs } from "../hooks/useFetchGifs";
-// import { GifGridItem } from "./GifGridItem";
-// import { getGifs } from "../helpers/getGifs";
+import { GifGridItem } from "./GifGridItem";
 
 export const GifGrid = ({ category }) => {
-	// const [images, setImages] = useState([]);
-	const { loading } = useFetchGifs();
-
-	// useEffect(() => {
-	// 	getGifs(category).then((imgs) => setImages(imgs));
-	// }, [category]);
+	const { data: images, loading } = useFetchGifs(category);
 
 	return (
 		<>
 			<h3>{category}</h3>
-			{loading ? "Cargando..." : "Fin de carga"}
-			{/* <div className="card-grid">
+			{loading && <p>Cargando...</p>}
+			<div className="card-grid">
 				{images.map((img) => (
 					<GifGridItem key={img.id} {...img} />
 				))}
-			</div> */}
+			</div>
 		</>
 	);
 };
